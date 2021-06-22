65e26ebff1a8eee08de1a207b29968137c32d2ae
Michał Marczuk <michal.marczuk0@gmail.com>
2021-06-22 16:38:22 +0200

    Added RentStatusService

---
+++ src/main/java/pl/mcm/carrental/service/implementation/RentStatusServiceImpl.java
@@ -0,0 +1,25 @@
+package pl.mcm.carrental.service.implementation;
+
+import org.springframework.stereotype.Service;
+import pl.mcm.carrental.model.RentStatus;
+import pl.mcm.carrental.repository.RentStatusRepository;
+import pl.mcm.carrental.service.RentStatusService;
+
+@Service
+public class RentStatusServiceImpl implements RentStatusService {
+    private final RentStatusRepository rentStatusRepository;
+
+    public RentStatusServiceImpl(RentStatusRepository rentStatusRepository) {
+        this.rentStatusRepository = rentStatusRepository;
+    }
+
+    @Override
+    public RentStatus addRentStatus(RentStatus rentStatus) {
+        return rentStatusRepository.save(rentStatus);
+    }
+
+    @Override
+    public RentStatus getRentStatusByName(String name) {
+        return rentStatusRepository.getById(name);
+    }
+}
